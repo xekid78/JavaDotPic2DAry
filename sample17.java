a348e4112d72f336a4851377e5c3f482d80864bc
Makoto Tanabe <xekid78@outlook.com>
2018-01-20 19:55:50 +0900

    二次元配列を使ったドット絵表示

---
+++ sample17.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class sample17 {
+
+	public static void main(String[] args) {
+		int[][] letterA =
+            {{0,0,1,1,0,0},
+             {0,1,0,0,1,0},
+             {1,0,0,0,0,1},
+             {1,1,1,1,1,1},
+             {1,0,0,0,0,1},
+             {1,0,0,0,0,1}};
+
+		for (int[] line : letterA) {
+			for (int dot : line) {
+				if (dot == 1) {
+					System.out.print("#");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
